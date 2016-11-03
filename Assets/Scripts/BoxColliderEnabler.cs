fd0268c4b14e736193e93217e1e361d65d5fe8b8
m4jr0 <lowgravityisfun@gmail.com>
2016-11-03 12:55:35 -0400

    Fixed Box Collider bug.

---
+++ Assets/Scripts/BoxColliderEnabler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxColliderEnabler : MonoBehaviour {
+	void Start () {
+        // Bug Fix: adding the BoxCollider2D directly in the
+        // editor prevented the Mouse Events from happening
+        this.gameObject.AddComponent<BoxCollider2D>();
+    }
+	
+	void Update () {
+	
+	}
+}
